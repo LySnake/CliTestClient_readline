8fc8092acafa127a2732753ed07ff8417029c0e9
LySnake <lysnake1024@gmail.com>
2020-12-08 23:16:46 +0800

    使用popen方法，从addr2line中的标准输出读到当前程序

--- Backtrace.cpp
+++ Backtrace.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
  
 constexpr size_t MAX_TRACE_DEEP = 20;
+constexpr size_t BUFF_SIZE = 1024;
 const std::string FILENAME = "a.out";
  
 void print_trace (void)
@@ -21,15 +22,26 @@ void print_trace (void)
         perror("backtrace_synbols");
         return;
     }
-    for(i = 0; i < size; i++) {
-        printf("--%s \n", strings[i]);
-    }
-
 
     for (i = 0; i < size; i++) {
+        char buf[BUFF_SIZE];
         std::stringstream cmd;
         cmd << "addr2line " << array[i] << " -e " << FILENAME << " -fisp";
-        system(cmd.str().c_str());
+        FILE *p_Pip = popen(cmd.str().c_str(), "r");
+        if(!p_Pip) {
+            perror("popen fail.");
+            continue;
+        }
+
+        char *str = fgets(buf, sizeof(buf), p_Pip);
+        if(str != buf) {
+            perror("fgets fail.");
+            pclose(p_Pip);
+            continue;
+        }
+
+        printf("%s", str);
+        pclose(p_Pip);
     }
 }
 
